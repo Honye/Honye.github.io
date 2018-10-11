2be68ed07cb411ca8dadf1ea9c99b716f9c2ecc3
Honye <hongye.jun@qq.com>
2018-10-11 15:53:40 +0800

    U: separate build.sh.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ev
+git clone https://${GH_REF} .deploy_git
+cd .deploy_git
+git checkout master
+cd ../
+mv .deploy_git/.git/ ./public/
+cd ./public/
+git add .
+git commit -m "Travis CI auto built at `date +"%Y-%m-%d %H:%M"`"
+# GitHub Pages
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
+# Coding Pages
+git push --force --quiet "https://Honye:${CODING_TOKEN}@${CODING_REF}" master:master
